55fd9c65a622bd0a2a8da75e628a9e2e0e463987
guptark661 <59641898+guptark661@users.noreply.github.com>
2020-01-08 13:58:57 +0545

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HOTEL-MANGEMENT-GUI-AND-NORMAL-PROGRAM-PYTHON--master',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Rahul',
+    author_email='',
+    description=''
+)
